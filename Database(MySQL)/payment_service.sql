592a49496ed950063eb9d10c56b1fb8a5fb3b0c1
Mohammad Nafis Naufally <32873358+MNafisN@users.noreply.github.com>
2019-05-07 20:36:53 +0700

    Rename library.sql to payment_service.sql

--- Database(MySQL)/payment_service.sql
+++ Database(MySQL)/payment_service.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (32 bit)
 MySQL - 5.6.20 : Database - payment_service
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
