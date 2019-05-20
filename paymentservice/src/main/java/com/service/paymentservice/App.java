14f1902ef53cc21ee91fb8162ee0eff5da597cd5
puguh1818 <puguh.sd@gmail.com>
2019-05-20 21:18:17 +0700

    new project

---
+++ paymentservice/src/main/java/com/service/paymentservice/App.java
@@ -0,0 +1,13 @@
+package com.service.paymentservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ paymentservice/src/main/java/com/service/paymentservice/model/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.service.paymentservice.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+    @Id
+    private String id;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("created_date")
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+//    public void setCreatedDate(Date createdDate) {
+//        this.createdDate = createdDate;
+//    }
+
+    @JsonProperty("last_modified_date")
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+//    public void setLastModifiedDate(Date lastModifiedDate) {
+//        this.lastModifiedDate = lastModifiedDate;
+//    }
+}
